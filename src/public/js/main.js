9421f73d0d495b4e5d6368fdc3b806535a9d0725
DevCesarHdez <root@localhost.localdomain>
2020-10-30 23:39:52 -0600

    Remover producto del carrito de compras

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,7 +5,9 @@ let products = [
 	{"id": 4, "name": "Memoria USB 32GB", "description": "Memoria USB, marca Kingston, capacidad 32GB, color verde", "price": 130, "selected": false},
 	{"id": 5, "name": "Memoria USB 64GB", "description": "Memoria USB, marca Kingston, capacidad 64GB, color negro", "price": 230, "selected": false},
 ]
-console.log(products);
+
+let selectedProducts = [];
+//console.log(products);
 
 if(!localStorage.getItem("Products")){
 	localStorage.setItem("Products", JSON.stringify(products));
@@ -23,7 +25,7 @@ if(formulario){
 			price: e.target.price.value,
 			selected: false
 		});
-		console.log(products);
+		//console.log(products);
 		localStorage.setItem("Products", JSON.stringify(products));
 		alert('A creado un nuevo producto');
 	});
@@ -46,3 +48,39 @@ const liProducts = JSON.parse(localStorage.getItem("Products")).map(product => {
 const containerProducts = document.querySelector('#products-list');
 if(containerProducts) containerProducts.innerHTML = liProducts;
 
+
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+if(checkboxes){
+	checkboxes.forEach((item, idx) => {
+		item.addEventListener('click', function(e){
+			const arr = item.id.split('-');
+			const id = parseInt(arr[1]);
+			const selectedProduct = JSON.parse(localStorage.getItem("Products"))[idx];
+			if(item.checked){
+				const cantidad = prompt("Cuantos productos desea agregar", 1); 
+				if(cantidad > 0){
+					//console.log(cantidad);
+					selectedProducts.push({
+						id: selectedProduct.id,
+						name: selectedProduct.name,
+						description: selectedProduct.description,
+						price: selectedProduct.price,
+						cantidad: cantidad
+					});
+					localStorage.setItem("SelectedProducts", JSON.stringify(selectedProducts));
+				}else{
+					item.checked = false;
+				}
+			}else{
+				if(confirm("¿Seguro que desea quitar el producto del carrito de compras?")){
+					selectedProducts = JSON.parse(localStorage.getItem("SelectedProducts")).filter((item, idx) => {
+						return item.id != id;
+					});
+					localStorage.setItem("SelectedProducts", JSON.stringify(selectedProducts));
+				}else{
+					item.checked = true;
+				}
+			}
+		});
+	});
+}
